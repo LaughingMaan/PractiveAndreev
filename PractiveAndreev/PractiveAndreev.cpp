ecfb12a3e229c9a3fec29f540e9b661b96ba1274
LaughingMaan <msu.remont@yandex.ru>
2021-04-13 10:20:39 +0300

    Добавьте файлы проекта.

---
+++ PractiveAndreev/PractiveAndreev.cpp
@@ -0,0 +1,14 @@
+﻿// PractiveAndreev.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World!\n";
+	argc > 1 ? printf("%s", argv[1]) : printf("%s", "No argv");
+	int arg = atoi(argv[1]);
+
+	return 0;
+}
+
